ef93986563268732166910dd43672cefac9777bf
Katsiaryna31 <katia310790@mail.ru>
2021-07-04 22:14:33 +0300

    feat: add ready task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  let newArr = array.sort((a,b) => {
+    return a-b;
+  })
+  return newArr[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  let newArr = array.sort((a,b) => {
+    return b-a;
+  })
+  return newArr[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+  let sum = array.reduce((a,b)=> {
+    return a+b;
+  })
+  return sum/array.length;
 }
